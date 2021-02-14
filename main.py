6e1ed774f7c186b36db689692163a71b44069480
Vimchenko <e.vimchenko@gmail.com>
2021-02-14 20:29:38 +0200

    Beta

--- main.py
+++ main.py
@@ -5,19 +5,45 @@ import pygame.locals
 import pygame.sprite
 # инициализация модулей для PyGame
 pygame.init()
+pygame.mixer.init()
 # настройка экрана
-W = 600
-H = 600
+W = 700
+H = 650
 # частота кадров в секунду
 FPS = 60
+# определили оттенок чёрного цвета (для текста)
+BLACK = (0, 0, 0)
 
 screen = pygame.display.set_mode((W, H))
-pygame.display.set_caption('OOP')  # отображение названия игры
-background = pygame.image.load('background.jpg')
-player_file = pygame.image.load('player.png')
-enemy_file = pygame.image.load('enemy.png')
-trap_file = pygame.image.load('trap.png')
+pygame.display.set_caption('FASTFIST')  # отображение названия игры
 clock = pygame.time.Clock()
+
+font_name = pygame.font.match_font('clarendon')  # шрифт
+
+
+# функция, в которой мы определили параметры текста
+def text_front(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, BLACK)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+
+# функция, отвечающая за содержимое экрана конца игры
+def screen_of_the_end():
+    screen.blit(background, [0, 0])
+    text_front(screen, "Дабы начать игру, нажмите любую клавишу!", 35, W / 2, H * 1 / 4)
+    text_front(screen, str(score), 50, W / 2, H * 2 / 3)
+    pygame.display.flip()
+    waiting = True
+    while waiting:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                waiting = False
 
 
 class Player(pygame.sprite.Sprite):
@@ -25,6 +51,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = player_file
         self.rect = self.image.get_rect()
+        self.radius = 10
         player_file.convert_alpha()
         # поялвение игрока в случайном месте экрана
         self.rect.x = random.randrange(0, W - self.rect.width)
@@ -39,13 +66,13 @@ class Player(pygame.sprite.Sprite):
         # реализация передвижения игрока (стрелки)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.speed_x = -8
+            self.speed_x = -14
         if keys[pygame.K_RIGHT]:
-            self.speed_x = 8
+            self.speed_x = 14
         if keys[pygame.K_UP]:
-            self.speed_y = -8
+            self.speed_y = -14
         if keys[pygame.K_DOWN]:
-            self.speed_y = 8
+            self.speed_y = 14
         # границы для передвижения игрока
         self.rect.x += self.speed_x
         if self.rect.right > W:
@@ -59,19 +86,49 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
 
 
+class Trap(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = trap_file
+        self.rect = self.image.get_rect()
+        self.radius = 6
+        trap_file.convert_alpha()
+
+        self.rect.x = random.randrange(0, W - self.rect.width)
+        self.rect.y = random.randrange(0, H - self.rect.height)
+
+        self.speed = 25
+
+        self.moving = True
+
+    def horizontal_movement(self):
+        if self.moving == True:
+            self.rect.move_ip(self.speed, 0)
+            if self.speed > 0 and self.rect.left > W:
+                self.rect.right = 0
+            if self.speed < 0 and self.rect.right < 0:
+                self.rect.left = W
+
+    def random(self):
+        self.rect.x = random.randrange(0, W - self.rect.width)
+        self.rect.y = random.randrange(0, H - self.rect.height)
+
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = enemy_file
         self.rect = self.image.get_rect()
+        self.radius = 25
         enemy_file.convert_alpha()
         # появление врага в случайном месте экрана
         self.rect.x = random.randrange(0, W - self.rect.width)
         self.rect.y = random.randrange(0, H - self.rect.height)
 
-        self.speed_x = 8
-        self.speed_y = 8
+        self.speed_x = 11
+        self.speed_y = 11
 
+    # следование за игроком
     def follow_player(self, player):
         dx, dy = player.rect.x - self.rect.x, player.rect.y - self.rect.y
         dist = math.hypot(dx, dy)
@@ -79,23 +136,36 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.x += dx * self.speed_x
         self.rect.y += dy * self.speed_y
 
+    # отдельная функция рслучайного появления Врага для цикла hit_3
+    def randomize(self):
+        self.rect.x = random.randrange(0, W - self.rect.width)
+        self.rect.y = random.randrange(0, H - self.rect.height)
 
-class Trap(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = trap_file
-        self.rect = self.image.get_rect()
-        trap_file.convert_alpha()
 
+background = pygame.image.load('background.jpg')
+player_file = pygame.image.load('player.png')
+enemy_file = pygame.image.load('enemy.png')
+trap_file = pygame.image.load('trap.png')
 
 all_sprites = pygame.sprite.Group()
 player = Player()
 enemy = Enemy()
 trap = Trap()
 all_sprites.add(player, enemy, trap)
-
+# счётчик
+score = 0
+# цикл активностей игрового процесса
+game_over = True
 running = True
 while running:
+    if game_over:
+        screen_of_the_end()
+        game_over = False
+        all_sprites = pygame.sprite.Group()
+        player = Player()
+        enemy = Enemy()
+        trap = Trap()
+        all_sprites.add(player, enemy, trap)
     # держим цикл на нормальной скорости
     clock.tick(FPS)
     # ввод процесса (события)
@@ -104,10 +174,32 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(background, [0, 0])  # избавляет от "следов" при передвижении игрока
     all_sprites.update()  # обновление (дабы игрок видел передвижение спрайта)
+    # соприкосновения между классами и циклы (if), отвечающие за результат от столкновений
+    hit = pygame.sprite.collide_circle(player, enemy)
+    hit_2 = pygame.sprite.collide_circle(player, trap)
+    hit_3 = pygame.sprite.collide_circle(enemy, trap)
+    if hit:
+        game_over = True
+    if hit_2:
+        game_over = True
+    if hit_3:
+        enemy.randomize()
+        trap.random()
+        score += 1
+
+    screen.blit(background, [0, 0])  # избавляет от "следов" при передвижении игрока
     all_sprites.draw(screen)   # отрисовка изображений на экране (для компьютера)
     enemy.follow_player(player)  # вызываем функию, отвечающую за слежение за игроком
+    trap.horizontal_movement()  # вызываем функцию, отвечающую за горизонтальное передвижение ловушки
     pygame.display.flip()  # отображение игры игроку
 
 pygame.quit()
+
+''' ДОРАБОТАТЬ: 
+   1) Обнуление счётчика
+   2) Передвижение Trap вверх-виз
+   3) Начало игры при нажатии на клавишу "SPACE"
+   4) Выход из игры при нажатии "Q"
+   5) Unittests + Docker 
+   '''
